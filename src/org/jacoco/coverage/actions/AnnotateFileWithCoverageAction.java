d952fba2c063cafcb0ce2f0190a9117eb4e822c2
Andreas Resios <andrei.resios@gmail.com>
2014-01-08 22:38:34 +0200

    Add notification when missing report while trying to view file coverage

--- src/org/jacoco/coverage/actions/AnnotateFileWithCoverageAction.java
+++ src/org/jacoco/coverage/actions/AnnotateFileWithCoverageAction.java
@@ -1,6 +1,10 @@
 package org.jacoco.coverage.actions;
 
 import com.intellij.ide.BrowserUtil;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationListener;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -9,12 +13,15 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.testIntegration.TestFinderHelper;
 import org.jacoco.coverage.util.CoverageUtils;
+import org.jetbrains.annotations.NotNull;
 
+import javax.swing.event.HyperlinkEvent;
 import java.io.File;
 import java.util.List;
 
 public class AnnotateFileWithCoverageAction extends AnAction {
 
+    private ActionManager actionManager = ActionManager.getInstance();
 
     @Override
     public void actionPerformed(AnActionEvent e) {
@@ -48,13 +55,43 @@ public class AnnotateFileWithCoverageAction extends AnAction {
                 String packageName = javaFile.getPackageName();
                 File url = new File(coverageRootPath, packageName);
                 VirtualFile virtualFile = javaFile.getVirtualFile();
+                boolean wasFound = false;
                 if (virtualFile != null) {
                     url = new File(url, virtualFile.getName() + ".html");
-                    if (url.exists()){
+                    if (url.exists()) {
                         BrowserUtil.launchBrowser(url.toURI().getPath());
+                        wasFound = true;
                     }
                 }
+                if (!wasFound) {
+                    Notifications.Bus.notify(getMissingFileNotification(virtualFile), project);
+                }
+            } else {
+                Notifications.Bus.notify(getMissingReportNotification(e), project);
             }
         }
+    }
+
+    private Notification getMissingFileNotification(VirtualFile url) {
+        String title = "File not found";
+        String content = "No coverage was found for " + (url != null ? url.getPresentableUrl() : "<null>");
+
+        return new Notification(CoverageUtils.PLUGIN_TITLE, title, content, NotificationType.ERROR);
+    }
+
+    private Notification getMissingReportNotification(final AnActionEvent e) {
+        return new Notification(
+                CoverageUtils.PLUGIN_TITLE,
+                "Coverage report missing",
+                "Please compile the report by clicking <a href='#'>here</a> and then retry.",
+                NotificationType.ERROR,
+                new NotificationListener() {
+                    @Override
+                    public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
+                        if (HyperlinkEvent.EventType.ACTIVATED.equals(event.getEventType())) {
+                            actionManager.tryToExecute(new GenerateCoverageReportAction(), e.getInputEvent(), null, null, true);
+                        }
+                    }
+                });
     }
 }
